aabc4574b621c87f00c07ffbffcabfeef44d3e60
bahmed721 <s.bilal.ahmed927@gmail.com>
2026-06-14 19:21:49 -0600

    Change data source from API to CSV

--- extraction.py
+++ extraction.py
@@ -1,10 +1,1 @@
-import requests
-import time
-
-url = "https://copa22.medeiro.tech"
-
-def retrieve(path : str):
-    req = requests.get(url + path)
-    req.raise_for_status()
-    return req.json()
-
+#Extraction Process
